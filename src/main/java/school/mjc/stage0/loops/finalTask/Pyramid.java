6b4af07c9ae5309a3d9caa5f12639c883c8aa8d8
ateistemes <timur.tynychbekov@gmail.com>
2023-10-18 21:50:39 +0600

    qwe

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,6 +3,22 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
 
+        for(int i = 1; i <= cathetusLength; i++){
+            //spaces;
+            for(int j = 1; j <= cathetusLength -i;j++){
+                System.out.print(' ');
+            }
+
+            //desc
+            for(int k = i; k >= 1; k--){
+                System.out.print(k);
+            }
+            //asce
+            for(int d = 2; d <= i; d++){
+                System.out.print(d);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
